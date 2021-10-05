00ddc30068b514e31fa04a6733a06da4b30991fa
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-10-05 11:14:42 -0600

    Fetch API

--- src/componentR/Rockets.js
+++ src/componentR/Rockets.js
@@ -1,11 +1,26 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getRock } from './redux/API';
 
-const Something = () => (
-  <div>
-    <h3>
-      Rockets
-    </h3>
-  </div>
-);
+const Rockets = () => {
+  const post = useSelector((state) => state.rock);
+  const dispatch = useDispatch();
+  const loadRocketsAction = bindActionCreators(getRock, dispatch);
+  useEffect(() => {
+    loadRocketsAction();
+  }, []);
+  return (
+    <div>
+      <ul>
+        {post.map((post) => (
+          <li key={post.id}>{post.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Something;
+export default Rockets;
---
+++ src/componentR/redux/API.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+/* eslint-disable arrow-body-style */
+import axios from 'axios';
+
+const GET_ROCKETS = 'ROCKETS';
+const url = 'https:/api.spacexdata.com/v3/rockets';
+const initialRockets = [];
+
+const reducer = (state = initialRockets, action) => {
+  switch (action.type) {
+    case GET_ROCKETS:
+      return [...action.newData];
+
+    default:
+      return state;
+  }
+};
+
+export const getRock = () => {
+  return (dispatch) => {
+    axios.get(url)
+      .then((res) => {
+        const allData = res.data;
+        const newData = [];
+        allData.forEach((element) => {
+          const item = {
+            id: element.id,
+            name: element.rocket_name,
+            type: element.rocket_type,
+            img: element.flickr_images[0],
+          };
+          newData.push(item);
+        });
+        dispatch({
+          type: GET_ROCKETS,
+          newData,
+        });
+      });
+  };
+};
+
+// id
+// rocket_name
+// rocket_type
+// flickr_images [0]
+
+export default reducer;
