592eb949615b019477f81632967ccea5220bf750
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-10-05 16:52:32 -0400

    Add spacing to mission title column

--- src/componentM/Mission.js
+++ src/componentM/Mission.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Mission = () => (
   <tr className="mission-item">
-    <td className="mission-title"><h3>Thaicom</h3></td>
+    <td className="mission-title"><h3 className="mission-title-heading">Thaicom</h3></td>
     <td className="mission-desc"><p /></td>
     <td className="mission-status"><h3 className="not-a-member">NOT A MEMBER</h3></td>
     <td className="mission-button"><button className="join-button" type="button">Join Mission</button></td>
--- src/componentM/Missions.js
+++ src/componentM/Missions.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Mission from './Mission';
 
 const SomethingM = () => (
-  <div>
-    <table>
+  <div className="missions-list">
+    <table className="missions-table">
       <tr>
-        <th><h3>Missions</h3></th>
+        <th><h3 className="mission-title-heading">Missions</h3></th>
         <th><h3>Descriptions</h3></th>
         <th><h3>Status</h3></th>
         <th> </th>
