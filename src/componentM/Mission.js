61bc91d9a11cfd41fc21f03e6a0df3c25a0da211
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-10-05 18:05:29 -0400

    Style missions table

--- src/componentM/Mission.js
+++ src/componentM/Mission.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Mission = () => (
   <tr className="mission-item">
-    <td className="mission-title"><h3 className="mission-title-heading">Thaicom</h3></td>
-    <td className="mission-desc"><p /></td>
-    <td className="mission-status"><h3 className="not-a-member">NOT A MEMBER</h3></td>
+    <td className="mission-title"><h4>Thaicom</h4></td>
+    <td className="mission-desc"><p>Thaicom is the name of a series of communications satellites operated from Thailand</p></td>
+    <td className="mission-status"><h4 className="not-a-member">NOT A MEMBER</h4></td>
     <td className="mission-button"><button className="join-button" type="button">Join Mission</button></td>
   </tr>
 );
--- src/componentM/Missions.js
+++ src/componentM/Missions.js
@@ -5,10 +5,10 @@ const SomethingM = () => (
   <div className="missions-list">
     <table className="missions-table">
       <tr>
-        <th><h3 className="mission-title-heading">Missions</h3></th>
-        <th><h3>Descriptions</h3></th>
-        <th><h3>Status</h3></th>
-        <th> </th>
+        <th><h4 className="mission-table-heading">Missions</h4></th>
+        <th className="mission-description"><h4 className="mission-table-heading">Descriptions</h4></th>
+        <th><h4 className="mission-table-heading">Status</h4></th>
+        <th><h4 className="mission-table-heading"> </h4></th>
       </tr>
       <Mission />
     </table>
